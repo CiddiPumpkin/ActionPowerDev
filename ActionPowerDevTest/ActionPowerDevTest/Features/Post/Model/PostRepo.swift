0565a8f68c50a7f1dd0ae3c5ece13aad6b624292
DavidChoi <choihs1026@gmail.com>
2026-02-05 14:19:45 +0900

    PostRepo 기능 추가(create, update, delete)

--- ActionPowerDevTest/ActionPowerDevTest/Features/Post/Model/PostRepo.swift
+++ ActionPowerDevTest/ActionPowerDevTest/Features/Post/Model/PostRepo.swift
@@ -10,6 +10,9 @@ import RxSwift
 
 protocol PostRepoType {
     func getPosts(page: Int, size: Int) -> Single<PostsResponse>
+    func createPost(title: String, body: String, userId: Int) -> Single<Post>
+    func updatePost(id: Int, title: String?, body: String?) -> Single<Post>
+    func deletePost(id: Int) -> Single<PostDeleteResponse>
 }
 
 final class PostRepo: PostRepoType {
@@ -23,5 +26,19 @@ final class PostRepo: PostRepoType {
         let page = max(0, page)
         let skip = page * size
         return postAPI.getPosts(limit: size, skip: skip)
+    }
+    
+    func createPost(title: String, body: String, userId: Int = 1) -> Single<Post> {
+        let request = PostCreateRequest(title: title, body: body, userId: userId)
+        return postAPI.createPost(req: request)
+    }
+    
+    func updatePost(id: Int, title: String? = nil, body: String? = nil) -> Single<Post> {
+        let request = PostUpdateRequest(title: title, body: body)
+        return postAPI.updatePost(id: id, req: request)
+    }
+    
+    func deletePost(id: Int) -> Single<PostDeleteResponse> {
+        return postAPI.deletePost(id: id)
     }
 }
